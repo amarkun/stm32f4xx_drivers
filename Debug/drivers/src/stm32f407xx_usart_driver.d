177e68cf10800f112113c15c89d404422262244b
Adam Markun <adambenmarkun@gmail.com>
2020-06-25 11:43:46 -0600

    minor changes to branch

---
+++ Debug/drivers/src/stm32f407xx_usart_driver.d
@@ -0,0 +1,23 @@
+drivers/src/stm32f407xx_usart_driver.o: \
+ ../drivers/src/stm32f407xx_usart_driver.c \
+ D:/Adam/Documents/Udemy/MasteringMicrocontrollerM1/CubeIDE/Workspace/stm32f4xx_drivers/drivers/inc/stm32f407xx_usart_driver.h \
+ D:/Adam/Documents/Udemy/MasteringMicrocontrollerM1/CubeIDE/Workspace/stm32f4xx_drivers/drivers/inc/stm32f407xx.h \
+ D:/Adam/Documents/Udemy/MasteringMicrocontrollerM1/CubeIDE/Workspace/stm32f4xx_drivers/drivers/inc/stm32f407xx_gpio_driver.h \
+ D:/Adam/Documents/Udemy/MasteringMicrocontrollerM1/CubeIDE/Workspace/stm32f4xx_drivers/drivers/inc/stm32f407xx_spi_driver.h \
+ D:/Adam/Documents/Udemy/MasteringMicrocontrollerM1/CubeIDE/Workspace/stm32f4xx_drivers/drivers/inc/stm32f407xx_i2c_driver.h \
+ D:/Adam/Documents/Udemy/MasteringMicrocontrollerM1/CubeIDE/Workspace/stm32f4xx_drivers/drivers/inc/stm32f407xx_rcc_driver.h \
+ D:/Adam/Documents/Udemy/MasteringMicrocontrollerM1/CubeIDE/Workspace/stm32f4xx_drivers/drivers/inc/stm32f407xx_usart_driver.h
+
+D:/Adam/Documents/Udemy/MasteringMicrocontrollerM1/CubeIDE/Workspace/stm32f4xx_drivers/drivers/inc/stm32f407xx_usart_driver.h:
+
+D:/Adam/Documents/Udemy/MasteringMicrocontrollerM1/CubeIDE/Workspace/stm32f4xx_drivers/drivers/inc/stm32f407xx.h:
+
+D:/Adam/Documents/Udemy/MasteringMicrocontrollerM1/CubeIDE/Workspace/stm32f4xx_drivers/drivers/inc/stm32f407xx_gpio_driver.h:
+
+D:/Adam/Documents/Udemy/MasteringMicrocontrollerM1/CubeIDE/Workspace/stm32f4xx_drivers/drivers/inc/stm32f407xx_spi_driver.h:
+
+D:/Adam/Documents/Udemy/MasteringMicrocontrollerM1/CubeIDE/Workspace/stm32f4xx_drivers/drivers/inc/stm32f407xx_i2c_driver.h:
+
+D:/Adam/Documents/Udemy/MasteringMicrocontrollerM1/CubeIDE/Workspace/stm32f4xx_drivers/drivers/inc/stm32f407xx_rcc_driver.h:
+
+D:/Adam/Documents/Udemy/MasteringMicrocontrollerM1/CubeIDE/Workspace/stm32f4xx_drivers/drivers/inc/stm32f407xx_usart_driver.h:
---
+++ Debug/src/012i2c_slave_tx_string_alt.d
@@ -0,0 +1,22 @@
+src/012i2c_slave_tx_string_alt.o: ../src/012i2c_slave_tx_string_alt.c \
+ D:/Adam/Documents/Udemy/MasteringMicrocontrollerM1/CubeIDE/Workspace/stm32f4xx_drivers/drivers/inc/stm32f407xx.h \
+ D:/Adam/Documents/Udemy/MasteringMicrocontrollerM1/CubeIDE/Workspace/stm32f4xx_drivers/drivers/inc/stm32f407xx_gpio_driver.h \
+ D:/Adam/Documents/Udemy/MasteringMicrocontrollerM1/CubeIDE/Workspace/stm32f4xx_drivers/drivers/inc/stm32f407xx.h \
+ D:/Adam/Documents/Udemy/MasteringMicrocontrollerM1/CubeIDE/Workspace/stm32f4xx_drivers/drivers/inc/stm32f407xx_spi_driver.h \
+ D:/Adam/Documents/Udemy/MasteringMicrocontrollerM1/CubeIDE/Workspace/stm32f4xx_drivers/drivers/inc/stm32f407xx_i2c_driver.h \
+ D:/Adam/Documents/Udemy/MasteringMicrocontrollerM1/CubeIDE/Workspace/stm32f4xx_drivers/drivers/inc/stm32f407xx_rcc_driver.h \
+ D:/Adam/Documents/Udemy/MasteringMicrocontrollerM1/CubeIDE/Workspace/stm32f4xx_drivers/drivers/inc/stm32f407xx_usart_driver.h
+
+D:/Adam/Documents/Udemy/MasteringMicrocontrollerM1/CubeIDE/Workspace/stm32f4xx_drivers/drivers/inc/stm32f407xx.h:
+
+D:/Adam/Documents/Udemy/MasteringMicrocontrollerM1/CubeIDE/Workspace/stm32f4xx_drivers/drivers/inc/stm32f407xx_gpio_driver.h:
+
+D:/Adam/Documents/Udemy/MasteringMicrocontrollerM1/CubeIDE/Workspace/stm32f4xx_drivers/drivers/inc/stm32f407xx.h:
+
+D:/Adam/Documents/Udemy/MasteringMicrocontrollerM1/CubeIDE/Workspace/stm32f4xx_drivers/drivers/inc/stm32f407xx_spi_driver.h:
+
+D:/Adam/Documents/Udemy/MasteringMicrocontrollerM1/CubeIDE/Workspace/stm32f4xx_drivers/drivers/inc/stm32f407xx_i2c_driver.h:
+
+D:/Adam/Documents/Udemy/MasteringMicrocontrollerM1/CubeIDE/Workspace/stm32f4xx_drivers/drivers/inc/stm32f407xx_rcc_driver.h:
+
+D:/Adam/Documents/Udemy/MasteringMicrocontrollerM1/CubeIDE/Workspace/stm32f4xx_drivers/drivers/inc/stm32f407xx_usart_driver.h:
